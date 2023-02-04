d6e80d2da22cc1709608837ab06544de11eb274d
kahajaah <120720099+kahajaah@users.noreply.github.com>
2023-02-04 08:54:59 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,22 +9,22 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    API_ID = int(getenv('API_ID', '22140808'))
+    API_HASH = str(getenv('API_HASH', '72e39b383c4655d957f67d4ab94cb3aa'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '5826172303:AAGT8uBzBFQSup8oxm8FejEJenP-Vixq4P4'))
     name = str(getenv('SESSION_NAME', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    PREMIUM_USERS = set(int(premium) for premium in os.environ.get("PREMIUM_USERS", "").split()) 
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    PREMIUM_USERS = set(int(premium) for premium in os.environ.get("PREMIUM_USERS", "1602928828 1004846223 5150795047").split()) 
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001608944668'))
     PORT = int(getenv('PORT', 8080))
-    WAIT_TIME = int(getenv('WAIT_TIME', 60))
+    WAIT_TIME = int(getenv('WAIT_TIME', 300))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "942099709").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', '@kwic2002'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -37,6 +37,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://kwictest:kwictest@cluster0.m8b69jo.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'MoviesNowOTT2'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
